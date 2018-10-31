ad6333a84284d1366cc5b3817999b862ac83a96c
vanditha <vn245@njit.edu>
2018-10-31 04:03:14 -0400

    Task : Contact form created and added to the main menu

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+
+@section('content')
+    <br>
+    <br>
+    <br>
+
+
+
+    <h1>This is my About page </h1>
+
+
+
+
+@endsection
+
+
+
---
+++ resources/views/pages/thankyou.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <br>
+    <h1>Thank you: {{$name}}</h1>
+@endsection
