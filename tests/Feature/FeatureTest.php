7bab1aad1b332e75ea8434700e984219d82cf73c
vanditha <vn245@njit.edu>
2018-10-31 21:39:53 -0400

    Task : Feature test-about

---
+++ tests/Feature/FeatureTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FeatureTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test1()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+    public function test2()
+    {
+    $response = $this->get('/login');
+
+    $response->assertStatus(200);
+    }
+    public function test3()
+    {
+        $response = $this->get('/about');
+
+        $response->assertStatus(200);
+    }
+}
