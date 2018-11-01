3aded167d69083bbdf5864941c3e6453a6e760c8
vanditha <vn245@njit.edu>
2018-10-31 22:34:43 -0400

    Task : unit test to insert a user

---
+++ tests/Unit/UnitInsertTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UnitInsertTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUnitInsert()
+    {
+        $user = factory(\App\User::class)->create();
+        $this->assertDatabaseHas('users', ['id' => $user->id]);
+    }
+}
